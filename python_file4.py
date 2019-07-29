8e701ef5dc62a80fa6e7c822320b9bc69b51c7c3
bgramesh76 <venkatarameshkumar76@gmail.com>
2019-07-29 14:58:17 +0530

    add this thingas

--- python_file4.py
+++ python_file4.py
@@ -1,1 +1,20 @@
-pyint("this is python file 4")
+#! /usr/bin/python3
+
+print("this is python file 4")
+
+users = ['val', 'bob', 'mia', 'ron', 'ned']
+num_users = len(users)
+print("We have " + str(num_users) + " users.")
+
+print(users.sort())
+print(users.sort(reverse=True))
+print(sorted(users))
+print(sorted(users, reverse=True))
+print(users.reverse())
+
+for user in users:
+print(user)
+
+for user in users:
+print("Welcome, " + user + "!")
+print("Welcome, we're glad to see you all!")
